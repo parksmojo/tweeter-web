28442c421bf144c699b77d00e857aafe43d4e646
Parker Shumard <parkersam@gmail.com>
2024-09-26 08:54:41 -0600

    Abstracted statusItem out of feed and story scrollers

--- tweeter-web/src/components/mainLayout/FeedScroller.tsx
+++ tweeter-web/src/components/mainLayout/FeedScroller.tsx
@@ -6,6 +6,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { Link } from "react-router-dom";
 import Post from "../statusItem/Post";
 import useToastListener from "../toaster/ToastListenerHook";
+import StatusItem from "../statusItem/StatusItem";
 
 export const PAGE_SIZE = 10;
 
@@ -17,11 +18,9 @@ const FeedScroller = () => {
   const [lastItem, setLastItem] = useState<Status | null>(null);
   const [changedDisplayedUser, setChangedDisplayedUser] = useState(true);
 
-  const addItems = (newItems: Status[]) =>
-    setNewItems(newItems);
-  
-  const { displayedUser, setDisplayedUser, currentUser, authToken } =
-    useContext(UserInfoContext);
+  const addItems = (newItems: Status[]) => setNewItems(newItems);
+
+  const { displayedUser, setDisplayedUser, currentUser, authToken } = useContext(UserInfoContext);
 
   // Initialize the component whenever the displayed user changes
   useEffect(() => {
@@ -30,17 +29,17 @@ const FeedScroller = () => {
 
   // Load initial items whenever the displayed user changes. Done in a separate useEffect hook so the changes from reset will be visible.
   useEffect(() => {
-    if(changedDisplayedUser) {
+    if (changedDisplayedUser) {
       loadMoreItems();
     }
   }, [changedDisplayedUser]);
 
   // Add new items whenever there are new items to add
   useEffect(() => {
-    if(newItems) {
+    if (newItems) {
       setItems([...items, ...newItems]);
     }
-  }, [newItems])
+  }, [newItems]);
 
   const reset = async () => {
     setItems([]);
@@ -48,25 +47,18 @@ const FeedScroller = () => {
     setLastItem(null);
     setHasMoreItems(true);
     setChangedDisplayedUser(true);
-  }
+  };
 
   const loadMoreItems = async () => {
     try {
-      const [newItems, hasMore] = await loadMoreFeedItems(
-        authToken!,
-        displayedUser!.alias,
-        PAGE_SIZE,
-        lastItem
-      );
+      const [newItems, hasMore] = await loadMoreFeedItems(authToken!, displayedUser!.alias, PAGE_SIZE, lastItem);
 
       setHasMoreItems(hasMore);
       setLastItem(newItems[newItems.length - 1]);
       addItems(newItems);
-      setChangedDisplayedUser(false)
+      setChangedDisplayedUser(false);
     } catch (error) {
-      displayErrorMessage(
-        `Failed to load feed items because of exception: ${error}`
-      );
+      displayErrorMessage(`Failed to load feed items because of exception: ${error}`);
     }
   };
 
@@ -80,39 +72,6 @@ const FeedScroller = () => {
     return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
   };
 
-  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
-
-    try {
-      const alias = extractAlias(event.target.toString());
-
-      const user = await getUser(authToken!, alias);
-
-      if (!!user) {
-        if (currentUser!.equals(user)) {
-          setDisplayedUser(currentUser!);
-        } else {
-          setDisplayedUser(user);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
-
   return (
     <div className="container px-0 overflow-visible vh-100">
       <InfiniteScroll
@@ -123,41 +82,8 @@ const FeedScroller = () => {
         loader={<h4>Loading...</h4>}
       >
         {items.map((item, index) => (
-          <div
-            key={index}
-            className="row mb-3 mx-0 px-0 border rounded bg-white"
-          >
-            <div className="col bg-light mx-0 px-0">
-              <div className="container px-0">
-                <div className="row mx-0 px-0">
-                  <div className="col-auto p-3">
-                    <img
-                      src={item.user.imageUrl}
-                      className="img-fluid"
-                      width="80"
-                      alt="Posting user"
-                    />
-                  </div>
-                  <div className="col">
-                    <h2>
-                      <b>
-                        {item.user.firstName} {item.user.lastName}
-                      </b>{" "}
-                      -{" "}
-                      <Link
-                        to={item.user.alias}
-                        onClick={(event) => navigateToUser(event)}
-                      >
-                        {item.user.alias}
-                      </Link>
-                    </h2>
-                    {item.formattedDate}
-                    <br />
-                    <Post status={item} />
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div key={index} className="row mb-3 mx-0 px-0 border rounded bg-white">
+            <StatusItem item={item} />
           </div>
         ))}
       </InfiniteScroll>
