b9295f20fe5695a79a827e753ee89777712929d8
Parker Shumard <parkersam@gmail.com>
2024-09-26 20:35:10 -0600

    Created user navigation hook to replace duplicated code

--- tweeter-web/src/components/statusItem/StatusItem.tsx
+++ tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -4,6 +4,7 @@ import { AuthToken, User, FakeData, Status } from "tweeter-shared";
 import { useContext } from "react";
 import useToastListener from "../toaster/ToastListenerHook";
 import { UserInfoContext } from "../userInfo/UserInfoProvider";
+import useUserNavigation from "../userInfo/userNavigationHook";
 
 interface Props {
   item: Status;
@@ -12,36 +13,7 @@ interface Props {
 const StatusItem = (props: Props) => {
   const { displayErrorMessage } = useToastListener();
   const { setDisplayedUser, currentUser, authToken } = useContext(UserInfoContext);
-
-  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
-
-    try {
-      const alias = extractAlias(event.target.toString());
-
-      const user = await getUser(authToken!, alias);
-
-      if (!!user) {
-        if (currentUser!.equals(user)) {
-          setDisplayedUser(currentUser!);
-        } else {
-          setDisplayedUser(user);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (authToken: AuthToken, alias: string): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
+  const { navigateToUser } = useUserNavigation();
 
   return (
     <div className="col bg-light mx-0 px-0">
--- tweeter-web/src/components/userItem/UserItem.tsx
+++ tweeter-web/src/components/userItem/UserItem.tsx
@@ -1,8 +1,9 @@
 import { AuthToken, FakeData, User } from "tweeter-shared";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserInfoContext } from "../userInfo/UserInfoProvider";
 import { useContext } from "react";
 import useToastListener from "../toaster/ToastListenerHook";
+import useUserNavigation from "../userInfo/userNavigationHook";
 
 interface Props {
   value: User;
@@ -10,53 +11,15 @@ interface Props {
 
 const UserItem = (props: Props) => {
   const { displayErrorMessage } = useToastListener();
-  const { setDisplayedUser, currentUser, authToken } =
-    useContext(UserInfoContext);
-
-  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
-
-    try {
-      const alias = extractAlias(event.target.toString());
-
-      const user = await getUser(authToken!, alias);
-
-      if (!!user) {
-        if (currentUser!.equals(user)) {
-          setDisplayedUser(currentUser!);
-        } else {
-          setDisplayedUser(user);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
+  const { setDisplayedUser, currentUser, authToken } = useContext(UserInfoContext);
+  const { navigateToUser } = useUserNavigation();
 
   return (
     <div className="col bg-light mx-0 px-0">
       <div className="container px-0">
         <div className="row mx-0 px-0">
           <div className="col-auto p-3">
-            <img
-              src={props.value.imageUrl}
-              className="img-fluid"
-              width="80"
-              alt="Posting user"
-            />
+            <img src={props.value.imageUrl} className="img-fluid" width="80" alt="Posting user" />
           </div>
           <div className="col">
             <h2>
@@ -64,10 +27,7 @@ const UserItem = (props: Props) => {
                 {props.value.firstName} {props.value.lastName}
               </b>{" "}
               -{" "}
-              <Link
-                to={props.value.alias}
-                onClick={(event) => navigateToUser(event)}
-              >
+              <Link to={props.value.alias} onClick={(event) => navigateToUser(event)}>
                 {props.value.alias}
               </Link>
             </h2>
